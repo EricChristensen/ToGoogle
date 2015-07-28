75f0f3ca2d6296cb11f284b8b955462541de254e
Jim Stanton <jim.stanton@cerner.com>
2015-07-28 17:35:59 -0500

    Fixed token post and get in services

--- Desktop/ToGoogle/www/js/services.js
+++ Desktop/ToGoogle/www/js/services.js
@@ -11,19 +11,27 @@ angular.module('starter.services', ['base64'])
 .service('Auth', function($http, $cookies, $base64){
     return {
 	post : function(path, userID, token, data) {
-	    var userID = userID
-	    var token  = token
 	    var authString = 'Basic ' + $base64.encode(userID + ':' + token);
-	    console.log("Auth string: " + authString);
-	    return $http.post(path, data, [{
-		headers: { 'Authorization': authString }
-	    }]);
+	    console.log("authstring: " + authString);
+	    return $http({
+		url: path,
+		method: 'POST',
+		headers: {
+		    'Authorization': authString
+		},
+		data: data
+	    });
 	},
 	get  : function(path, userID, token) {
 	    var userID = userID;
 	    var token  = token;
-	    return $http.get(path, {
-		//headers: { 'Authorization': 'Basic ' + $base64.encode(userID + ':' + token) }
+	    return $http({
+		url: path,
+		method: 'GET',
+		data: data,
+		headers: {
+		    'Authorization': 'Basic ' + $base64.encode(userID + ':' + token)
+		}
 	    });
 	}
 	
